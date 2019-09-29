116ad87105d1949dc8f3ded9b719bc1c8aa2946e
yangzhen <y951477643@163.com>
2019-09-29 14:49:30 +0800

    window

--- start.php
+++ start.php
@@ -13,7 +13,7 @@ if(strpos(strtolower(PHP_OS), 'win') === 0)
 }
 
 // 检查扩展
-if(!extension_loaded('pcntl'))
+if(!extension_loaded('pcntl')
 {
     exit("Please install pcntl extension. See http://doc3.workerman.net/appendices/install-extension.html\n");
 }
